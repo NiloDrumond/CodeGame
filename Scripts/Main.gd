0c9ffed4b9d6110e6ef1f16fa875730cd9bf46c0
Nilo <nilobdrumond@gmail.com>
2019-09-28 19:00:51 -0300

    loop

--- Scripts/Main.gd
+++ Scripts/Main.gd
@@ -11,6 +11,24 @@ var constVariables = {
 	"false": false,
 }
 
+var builtFunctions = {
+	"print" : funcref(self, "print_function")
+	
+}
+
+var functions = {}
+
+func print_function(arguments):
+	var printContent = arguments[0]
+	output.newline()
+	if variables.has(printContent):
+		output.add_text(str(variables[printContent]))
+	elif constVariables.has(printContent):
+		output.add_text(str(constVariables[printContent]))
+	else:
+		output.add_text(str(printContent))
+	
+
 func _ready():
 	pass
 
@@ -38,7 +56,7 @@ func crash(error, line):
 	output.add_text("Error: " + error + ". For line: " + str(line))
 
 func create_variable(line):
-	var varName = line.split(" ")[1]
+	var varName = line.split(" ", false, 2)[1]
 	for i in variables:
 		if i == varName:
 			crash("variable already exists", line)
@@ -47,6 +65,127 @@ func create_variable(line):
 		var assignmentLine = line.replace("var ", "")
 		process_assignment(assignmentLine)
 		
+	
+func get_end_bracket(startIndex):
+	for i in range(startIndex, codeEnd):
+		if console.get_line(i) == "}":
+			return i + 1
+			
+	crash("Expecting } ", startIndex)
+
+func get_chain_end_line(startIndex):
+	for i in range(startIndex, codeEnd):
+		if console.get_line(i)[0] == "":
+			pass
+		elif console.get_line(i).split(" ")[0] == "elif" or console.get_line(i).split(" ")[0] == "else":
+			var newIndex = get_end_bracket(i)
+			return get_chain_end_line(newIndex)
+		else:
+			return i
+
+func look_for_else(startIndex):
+	for i in range(startIndex, codeEnd):
+		if console.get_line(i) == "":
+			pass
+		elif console.get_line(i).split(" ")[0] == "elif" or console.get_line(i).split(" ")[0] == "else":
+			return i
+		else:
+			return -1
+
+func handle_condition(expression, lineIndex, stopIndex):
+	var runCondition = get_condition_result(expression)
+	
+	var stopConditionIndex = get_end_bracket(lineIndex)
+	
+	if runCondition:
+		run_line(lineIndex + 1, stopConditionIndex)
+		var endChainIndex = get_chain_end_line(stopConditionIndex)
+		run_line(endChainIndex, stopIndex)
+	else:
+		var nextConditionIndex = look_for_else(stopConditionIndex)
+		if nextConditionIndex == null or nextConditionIndex == -1:
+			run_line(stopConditionIndex, stopIndex)
+		elif console.get_line(nextConditionIndex).split(" ")[0] == "elif":
+			var newExpression = console.get_line(nextConditionIndex).replace("elif ", "").replace(" {","")
+			handle_condition(newExpression, stopConditionIndex, stopIndex)
+		else:
+			run_line(stopConditionIndex+1, stopIndex)
+
+func read_function(expression, lineIndex):
+	var f = expression.split("(", true, 1)[0]
+	var arguments = expression.split("(", true, 1)[1].rsplit(")", true, 1)[0].split(",")
+	if builtFunctions.has(f):
+		return builtFunctions[f].call_func(arguments)
+	else:
+		crash("invalid function", lineIndex)
+	
+
+func handle_loop(lineIndex, stopIndex):
+	var line = console.get_line(lineIndex)
+	var content = line.split(" ")[1]
+	var loopEnd = get_end_bracket(lineIndex)
+	var iterations
+	if variables.has(content):
+		iterations = variables[content]
+	elif constVariables.has(content):
+		iterations = constVariables[content]
+	elif content.is_valid_integer():
+		iterations = content
+	else:
+		crash("invalid loop argument", lineIndex)
+		
+	for i in range(iterations):
+		run_line(lineIndex+1, loopEnd)
+		
+	run_line(loopEnd, stopIndex)
+		
+	
+
+func run_line(lineIndex, stopIndex):
+
+	if lineIndex == null or lineIndex > stopIndex:
+		pass
+	elif console.get_line(lineIndex) == "" or console.get_line(lineIndex) == "}":
+		run_line(lineIndex+1, stopIndex)
+	else:
+		var line = console.get_line(lineIndex)
+		
+		if line[0] == "	":
+			line = line.replace("	", "")
+		
+		var sections = line.split(" ", false, 1)
+		
+		if sections[0] == "var":
+			create_variable(line)
+			run_line(lineIndex+1, stopIndex)
+			
+		elif sections.size() > 1 and sections[1] == "=":
+			process_assignment(line)
+			run_line(lineIndex+1, stopIndex)
+			
+		elif sections[0] == "if" or sections[0] == "elif":
+			var expression = line.replace("if ", "").replace("elif ", "").replace("else ", "").replace(" {","")
+			handle_condition(expression, lineIndex, stopIndex)
+		
+		elif sections[0] == "loop":
+			handle_loop(lineIndex, stopIndex)
+			
+		else:
+			for f in builtFunctions:
+				if sections[0].begins_with(f):
+					read_function(line, lineIndex)
+					run_line(lineIndex+1, stopIndex)
+			for f in functions:
+				if sections[0].begins_with(f):
+					read_function(line, lineIndex)
+					run_line(lineIndex+1, stopIndex)
+	
+	
+func _on_RunButton_pressed():
+	variables.clear()
+	codeEnd = console.get_line_count()
+	run_line(0, codeEnd)
+
 func get_condition_result(expression):
 	var sections = expression.split(" ")
 	var members = []
@@ -151,84 +290,3 @@ func get_condition_result(expression):
 		else:
 			pass #Erro		
 	return members[0]
-	
-func get_end_bracket(startIndex):
-	for i in range(startIndex, codeEnd):
-		if console.get_line(i) == "}":
-			return i + 1
-			
-	crash("Expecting } ", startIndex)
-
-func get_chain_end_line(startIndex):
-	for i in range(startIndex, codeEnd):
-		if console.get_line(i)[0] == "":
-			pass
-		elif console.get_line(i).split(" ")[0] == "elif" or console.get_line(i).split(" ")[0] == "else":
-			var newIndex = get_end_bracket(i)
-			return get_chain_end_line(newIndex)
-		else:
-			return i
-
-func look_for_else(startIndex):
-	for i in range(startIndex, codeEnd):
-		if console.get_line(i) == "":
-			pass
-		elif console.get_line(i).split(" ")[0] == "elif" or console.get_line(i).split(" ")[0] == "else":
-			return i
-		else:
-			return -1
-
-func handle_condition(expression, lineIndex, stopIndex):
-	var runCondition = get_condition_result(expression)
-	
-	var stopConditionIndex = get_end_bracket(lineIndex)
-	
-	if runCondition:
-		run_line(lineIndex + 1, stopConditionIndex)
-		var endChainIndex = get_chain_end_line(stopConditionIndex)
-		run_line(endChainIndex, stopIndex)
-	else:
-		var nextConditionIndex = look_for_else(stopConditionIndex)
-		if nextConditionIndex == null or nextConditionIndex == -1:
-			run_line(stopConditionIndex, stopIndex)
-		elif console.get_line(nextConditionIndex).split(" ")[0] == "elif":
-			var newExpression = console.get_line(nextConditionIndex).replace("elif ", "").replace(" {","")
-			handle_condition(newExpression, stopConditionIndex, stopIndex)
-		else:
-			run_line(stopConditionIndex, stopIndex)
-
-func run_line(lineIndex, stopIndex):
-	if lineIndex == null or lineIndex > stopIndex:
-		pass
-	elif console.get_line(lineIndex) == "" or console.get_line(lineIndex) == "}":
-		run_line(lineIndex+1, stopIndex)
-	else:
-		var line = console.get_line(lineIndex)
-		
-		if line[0] == "	":
-			line = line.replace("	", "")
-		
-		var sections = line.split(" ")
-		
-		if sections[0] == "var":
-			create_variable(line)
-			run_line(lineIndex+1, stopIndex)
-			
-		elif sections[1] == "=":
-			process_assignment(line)
-			run_line(lineIndex+1, stopIndex)
-			
-		elif sections[0] == "print":
-			output.newline()
-			output.add_text(str(variables[sections[1]]))
-			run_line(lineIndex+1, stopIndex)
-			
-		elif sections[0] == "if" or sections[0] == "elif"  or sections[0] == "else":
-			var expression = line.replace("if ", "").replace("elif ", "").replace("else ", "").replace(" {","")
-			handle_condition(expression, lineIndex, stopIndex)
-	
-	
-func _on_RunButton_pressed():
-	variables.clear()
-	codeEnd = console.get_line_count()
-	run_line(0, codeEnd)
--- Scripts/TextEdit.gd
+++ Scripts/TextEdit.gd
@@ -2,14 +2,17 @@ extends TextEdit
 
 var varColor = Color( 1, 0.65, 0, 1 )
 var numberColor = Color("#faf09b")
-var funcColor = Color( 0.8, 0.36, 0.36, 1 )
+var primitiveFuncColor = Color( 0.8, 0.36, 0.36, 1 )
+var funcColor = Color( 0.12, 0.56, 1, 1 )
 
-var funcKeywords = ["if", "elif", "else", "print"]
+var primitiveFuncKeywords = ["if", "elif", "else", "loop"]
+var funcKeywords = ["print"]
 
 func _ready():
 	theme.set_color("number_color", "TextEdit",  numberColor) 
-	#add_color_region('!', '~', Color( 0, 0, 0, 1 ), true)
-
 	
+	for keyword in primitiveFuncKeywords:
+		add_keyword_color(keyword, primitiveFuncColor)
+		
 	for keyword in funcKeywords:
 		add_keyword_color(keyword, funcColor)
